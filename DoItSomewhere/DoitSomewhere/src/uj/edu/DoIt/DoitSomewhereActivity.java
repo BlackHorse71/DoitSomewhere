18b5efb9abef861cbd07d2b55383bed1114b8aa4
Krzysztof Palka <kpinfoclub@gmail.com>
2011-12-21 00:59:58 +0100

    ok1

---
+++ DoItSomewhere/DoitSomewhere/src/uj/edu/DoIt/DoitSomewhereActivity.java
@@ -0,0 +1,13 @@
+package uj.edu.DoIt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DoitSomewhereActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
