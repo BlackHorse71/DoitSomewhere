3c3d40bb6ac22117636d2599b18a629237845507
Krzysztof Palka <kpinfoclub@gmail.com>
2012-01-11 21:51:51 +0100

    Layout cont. today

--- DoItSomewhere/DoitSomewhere/gen/uj/edu/DoIt/R.java
+++ DoItSomewhere/DoitSomewhere/gen/uj/edu/DoIt/R.java
@@ -17,6 +17,8 @@ public final class R {
         public static final int TableLayout=0x7f050000;
         public static final int TableRow=0x7f050001;
         public static final int TextView=0x7f050002;
+        public static final int lat=0x7f050003;
+        public static final int lol=0x7f050004;
     }
     public static final class layout {
         public static final int locate=0x7f030000;
